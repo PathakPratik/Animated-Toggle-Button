4c92993e8a44a0fe89477f059e82e7e649910364
Pratik Pathak <rajpratik10@gmail.com>
2019-07-14 00:36:03 +0530

    Update Button.js

--- Button.js
+++ Button.js
@@ -1,13 +1,17 @@
 var toggleBtn = document.getElementById('toggleBtn');
 var openCloseBtn = document.getElementById('open-close');
 
-document.getElementById("control").addEventListener("click", function(e) {
-
-  toggleBtn.classList.toggle('closedBtn');
+document.addEventListener("DOMContentLoaded", function(){
   
-  if (toggleBtn.classList.contains('closedBtn'))
-    openCloseBtn.innerHTML = '&#10010;';
-  else
-    openCloseBtn.innerHTML = '&#10006;';
-    
+  document.getElementById("control").addEventListener("click", function(e) {
+
+    toggleBtn.classList.toggle('closedBtn');
+
+    if (toggleBtn.classList.contains('closedBtn'))
+      openCloseBtn.innerHTML = '&#10010;';
+    else
+      openCloseBtn.innerHTML = '&#10006;';
+
+  });
+  
 });
